40026f08cbf4c76df4f5c4996f66e7fc6fbbacce
CryptoThibault <thibault.chalaoux@gmail.com>
2021-03-04 19:41:10 +0100

    Bonjour

---
+++ crackmebonjour.js
@@ -0,0 +1,7 @@
+let password = ''
+
+function bonjour(xxxx) {
+  let coucou = []; for (hola = 2; hola < xxxx + 1; hola++) {
+    var hey = true; for (hi = 0; hi < coucou.length; hi++) { if (hola % coucou[hi] === 0) { hey += false } }; if (hey === true) { coucou.push(hola) }
+  }; if (password.length < ' '.charCodeAt()) { coucou.push('Hello') }; return xxxx === coucou[coucou.length - 1]
+}; let passwrod = -3; for (bye = 0; bye < password.length; bye++) { passwrod += password[bye].charCodeAt() }; console.log(bonjour(passwrod) ? 'GOOD' : 'BAD')
